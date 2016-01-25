3c63b59cd96b6423a52ad174249892b958c8ebc2
Brian Eng <brian@brianeng.com>
2016-01-24 20:23:52 -0600

    first commit

---
+++ 2.js
@@ -0,0 +1,13 @@
+// This code runs when the page loads
+$(function() {
+
+  // Add the rules() method to alert something incredible
+  var thisClass = {
+    name: "KIEI-924",
+    description: "The best class ever",
+    instructor: { name: "Brian", status: "Awesome" }
+  }
+
+  thisClass.rules();
+
+})
